702393d8a697ea987a994b177c5a6e023af2b233
lampkid <lampkid@qq.com>
2018-05-29 17:57:41 +0800

    feat: a scaffold to create npm package

---
+++ examples/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Pkg from '../src';
+
+import "./index.less";
+
+ReactDOM.render(
+    <div>
+       <Pkg /> 
+    </div>
+, document.getElementById('main'));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Pkg() {
+    return <div>Pkg</div>
+}
+export {Pkg};
+export default Pkg;
